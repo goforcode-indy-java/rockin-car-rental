fd2478471f54dbb6c6ebd251f92858f8c2c4bb09
Curtis Schlak <curtis.schlak@theironyard.com>
2017-11-12 14:17:08 -0500

    update project name and remove unused import.

--- src/main/java/com/lmig/gfc/rentals/controllers/RentalController.java
+++ src/main/java/com/lmig/gfc/rentals/controllers/RentalController.java
@@ -1,7 +1,5 @@
 package com.lmig.gfc.rentals.controllers;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
