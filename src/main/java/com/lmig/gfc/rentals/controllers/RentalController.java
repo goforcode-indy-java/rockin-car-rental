d388b1287879d785f5f189e9b8137bf4a1aa326a
Curtis Schlak <curtis.schlak@theironyard.com>
2017-11-12 09:25:20 -0800

    Initial commit of near MVP.

---
+++ src/main/java/com/lmig/gfc/rentals/controllers/RentalController.java
@@ -0,0 +1,82 @@
+package com.lmig.gfc.rentals.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lmig.gfc.rentals.models.Car;
+import com.lmig.gfc.rentals.models.Fleet;
+
+@Controller
+public class RentalController {
+	
+	private Fleet fleet;
+	
+	public RentalController() {
+		fleet = new Fleet();
+	}
+
+	@RequestMapping("/")
+	public ModelAndView showMainPage() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("main");
+		mv.addObject("fleet", fleet);
+		mv.addObject("showLowCarError", fleet.getAvailableCars().size() < 5);
+		return mv;
+	}
+	
+	@RequestMapping("/rent")
+	public ModelAndView rentCar(int index) {
+		fleet.makeCarUnavailable(index - 1);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/");
+		return mv;
+	}
+
+	@RequestMapping("/return")
+	public ModelAndView returnCar(int index) {
+		fleet.makeCarAvailable(index - 1);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("redirect:/");
+		return mv;
+	}
+	
+	@RequestMapping("/add_car")
+	public ModelAndView showAddCarForm(String make, String model, String year) {
+		if (make != null && !make.isEmpty()
+				&& model != null && !model.isEmpty()
+				&& year != null && !year.isEmpty()) {
+
+			Car car = new Car(make, model, year);
+			fleet.addCar(car);
+			
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("redirect:/");
+			return mv;
+			
+		}
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("addCarForm");
+		return mv;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/lmig/gfc/rentals/models/Car.java
@@ -0,0 +1,27 @@
+package com.lmig.gfc.rentals.models;
+
+public class Car {
+
+	private String make;
+	private String model;
+	private String year;
+	
+	public Car(String make, String model, String year) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public String getYear() {
+		return year;
+	}
+	
+}
